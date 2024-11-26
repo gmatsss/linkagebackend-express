79c06a25275a745af2ca498d57bdfaf7a23975b0
gmatsss <gmaturan60@gmail.com>
2024-11-26 17:57:02 +0800

    new endpoint

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ app.get("/health", (req, res) => {
   res.status(200).send("Healthy"); //health
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`The Server is running on port ${PORT}`);
 });
