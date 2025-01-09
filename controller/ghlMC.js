3770c9e36de82e6f68fc91a2f7968341cb687c23
gmatsss <gmaturan60@gmail.com>
2025-01-09 09:49:46 +0800

    update form submission

--- controller/ghlMC.js
+++ controller/ghlMC.js
@@ -183,7 +183,74 @@ const reSchedMCappointment = async (req, res) => {
   }
 };
 
+const Mcformsubmission = async (req, res) => {
+  try {
+    const { name, email, phone, formid } = req.body;
+
+    console.log(req.body);
+
+    const config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: `https://rest.gohighlevel.com/v1/forms/submissions?page=1&limit=1&formId=${formid}&q=${email}`,
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2NhdGlvbl9pZCI6IjYya1owQ1FxTW90Uld2ZElqTVpTIiwiY29tcGFueV9pZCI6IkkxTFUyYW1aSHpQWWo2YUdXMlRCIiwidmVyc2lvbiI6MSwiaWF0IjoxNjYxMzY5OTgwMjE4LCJzdWIiOiJrVGtad2R0TWdoUHlSYWY2c0F5QSJ9.19uamtaDwx8wJ9td8gZMsmIyGNZbhL3xp3SsjjYR12Y",
+      },
+    };
+
+    const response = await axios.request(config);
+
+    if (response.data && response.data.submissions.length > 0) {
+      const latestSubmission = response.data.submissions[0];
+      const {
+        name,
+        email,
+        createdAt,
+        full_name,
+        organization,
+        phone,
+        XFXzbGFzOH3dzvG6N1hE,
+        jQzA3agN1c1EofZmpH2a,
+        FdccxS3LYfhGNN2ODIjZ,
+        wV5gAq9TPL9tp2UQvWKX,
+        terms_and_conditions,
+      } = latestSubmission;
+
+      const filteredData = {
+        name,
+        email,
+        createdAt,
+        full_name,
+        organization,
+        phone,
+        answer_1: XFXzbGFzOH3dzvG6N1hE,
+        answer_2: jQzA3agN1c1EofZmpH2a,
+        answer_3: FdccxS3LYfhGNN2ODIjZ,
+        consent: wV5gAq9TPL9tp2UQvWKX,
+        terms_and_conditions,
+      };
+
+      res.status(200).json({
+        message: "Latest form submission retrieved successfully",
+        data: filteredData,
+      });
+    } else {
+      res.status(404).json({
+        message: "No submissions found for the provided form and email",
+      });
+    }
+  } catch (error) {
+    console.error("Error fetching form submissions:", error);
+    res.status(500).json({
+      message: "Failed to fetch form submissions",
+      error: error.message,
+    });
+  }
+};
+
 module.exports = {
   MCappointment,
   reSchedMCappointment,
+  Mcformsubmission,
 };
--- routes/ghlMC.js
+++ routes/ghlMC.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { MCappointment, reSchedMCappointment } = require("../controller/ghlMC");
+const {
+  MCappointment,
+  reSchedMCappointment,
+  Mcformsubmission,
+} = require("../controller/ghlMC");
 
 router.post("/setappointment", MCappointment);
 router.post("/resched", reSchedMCappointment);
+router.post("/formsubmission", Mcformsubmission);
 
 module.exports = router;
